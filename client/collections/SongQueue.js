4b5a915c86a8aac392780d8ab85edd9a50248c23
pbhorjee <paul@bhorjee.com>
2015-10-21 19:27:14 -0500

    working through specs

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -2,6 +2,17 @@
 var SongQueue = Songs.extend({
 
   initialize: function(){
+    this.on('add', function(model, collection, options){
+      if (collection.length === 1) {
+        this.playFirst(collection.at(0));
+      }
+    });
+
+  },
+
+  playFirst: function(song) {
+    song.trigger('play');
   }
+
 
 });
